a2ac8f1fd077dd41a2c5bfcc8bf13ee64b9976ce
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-12-11 15:30:47 +0100

    add new logo and set navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/">
+                        <img src="../logo_recensando.jpg" alt="logo" />
+                    </NavLink>
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul className="navbar-nav ms-auto mt-2 mt-lg-0 py-4 align-self-end">
+                            <li className="nav-item">
+                                <NavLink className="nav-link pe-5 fw-bold" to="/" aria-current="page">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link fw-bold" to="/Contacts">Contacts</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav >
+        </>
+    )
+}
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function DefaultLayout() {
 
     return (
         <>
-            <header>ciao header</header>
+            <Header />
             <Outlet />
-            <footer>ciao footer</footer>
+            <Footer />
         </>
     )
 }
